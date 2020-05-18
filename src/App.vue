d9c1eca63d34970b7624ba68736a86ca4f419598
mengxiangcheng2019 <572176737@qq.com>
2020-05-18 23:47:30 +0800

    '首页开发'

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div id="app" class="wrapper">
+    <router-view/>
+
+    <main-tab-bar/>
+  </div>
+</template>
+
+<script>
+import MainTabBar from 'components/content/MainTabBar'
+// import Home from 'views/home/Home'
+
+
+export default {
+  name: 'app',
+  components: {
+    MainTabBar,
+    // Home
+  }
+}
+</script>
+
+<style>
+@import "./assets/css/base.css";
+</style>
---
+++ src/network/home.js
@@ -0,0 +1,23 @@
+import {request} from "./request";
+export function getHomeMultidata() {
+  return request({
+    url: '/home/multidata'
+  })
+}
+
+export function getHomeGoods(type,page) {
+  return request({
+    url: '/home/data',
+    params: {
+      type,
+      page
+    }
+  })
+
+}
+
+let totalNums = []
+const nums1 = [20,11,222]
+const nums2 = [111,22,333]
+
+totalNums.push(...nums1)
